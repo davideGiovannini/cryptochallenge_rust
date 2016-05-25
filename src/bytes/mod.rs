ddc1d25046c57507641efcbd787b5c4389931324
Davide Giovannini <giovannini.davide90@gmail.com>
2016-05-25 13:33:53 +0200

    Added hamming distance

--- src/bytes/mod.rs
+++ src/bytes/mod.rs
@@ -24,14 +24,39 @@ pub fn xor_byte(bytes_seq: &[u8], byte: u8) -> Vec<u8> {
     result
 }
 
+/// Computes the Hamming distance between two byte sequences of the same lenght.
+///
+/// The Hamming distance in this case is just the number of differing bits.
+pub fn hamming_bit_distance(bytesA: &[u8], bytesB: &[u8]) -> usize {
+    assert_eq!(bytesA.len(), bytesB.len());
+    let mut distance = 0;
+
+    for (byteA, byteB) in bytesA.iter().zip(bytesB) {
+        if byteA != byteB {
+            for pos in 0..8 {
+                if byteA & (1 << pos) != byteB & (1 << pos) {
+                    distance += 1;
+                }
+            }
+        }
+    }
+
+    distance
+}
 
 #[cfg(test)]
 mod bytes_tests {
-    use super::xor_seqs;
+    use super::*;
 
     #[test]
     fn test_xor() {
         assert_eq!(xor_seqs(&vec![0xF0, 0x0F], &vec![0x0F, 0x0F]),
                    vec![0xFF, 0x00]);
+    }
+
+    #[test]
+    fn test_hamming() {
+        assert_eq!(hamming_bit_distance("this is a test".as_bytes(), "wokka wokka!!!".as_bytes()),
+                   37);
     }
 }
